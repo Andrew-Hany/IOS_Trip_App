a9e33a3ee279839702062fcfaadf746708912cb8
Andrew-Hany <andrewhany@aucegypt.edu>
2021-04-08 00:45:47 +0200

    Initial Commit

---
+++ first_swift_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first_swift_app
+//
+//  Created by auc on 4/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first_swift_app/first_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_swift_appApp.swift
+//  first_swift_app
+//
+//  Created by auc on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct first_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
